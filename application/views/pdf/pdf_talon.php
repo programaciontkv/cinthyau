2ed65196884386073eb1b061adda97a86aa6e3f1
programaciontkv <programacion@tikvas.com>
2023-07-07 08:30:01 -0500

    Reduccion de iva en factura y nota de credito

--- application/views/pdf/pdf_talon.php
+++ application/views/pdf/pdf_talon.php
@@ -239,7 +239,7 @@
                         <td id="inv" colspan="2" rowspan="4" valign="top" >
                             
                         </td>
-                        <td colspan="2"><strong>Subtotal 12%</strong></td>
+                        <td colspan="2"><strong>Subtotal <?php echo $t_iva ?>%</strong></td>
                         <td class="numerico"><?php echo number_format($factura->fac_subtotal12,$dec)?></td>
                     </tr>
                     <tr>
@@ -263,7 +263,7 @@
                         <td class="numerico"><?php echo number_format($factura->fac_total_descuento,$dec)?></td>
                     </tr> -->
                     <tr>
-                        <td colspan="2"><strong>IVA 12%</strong></td>
+                        <td colspan="2"><strong>IVA<?php echo $t_iva ?>%</strong></td>
                         <td class="numerico"><?php echo number_format($factura->fac_total_iva,$dec)?></td>
                     </tr>
                     <!-- <tr>
